7e3c7725eb3c0f6fa3f0a957c2f7cb733509161c
Oleg Kitaev <oleg.kitaev61@yandex.ru>
2025-03-05 22:43:26 +0300

    Copy files

---
+++ server/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -0,0 +1,65 @@
+package ru.practicum.shareit.booking;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingDtoInput;
+
+import java.util.Collection;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/bookings")
+public class BookingController {
+    private final BookingService bookingService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookingDto createBooking(@RequestBody BookingDtoInput dto,
+                                    @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на добавление нового бронирования вещи c id = {} у пользователя с id = {}",
+                dto.getItemId(), userId);
+        return bookingService.createBooking(userId, dto);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public BookingDto updateBooking(@RequestParam Boolean approved,
+                                    @PathVariable Long bookingId,
+                                    @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на подтверждение или отклонение бронирования с id = {}", bookingId);
+        return bookingService.updateBooking(userId, bookingId, approved);
+    }
+
+    @GetMapping("/{bookingId}")
+    public BookingDto getBookingById(@PathVariable Long bookingId,
+                                     @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на получение бронирования с id = {} от пользователя с id = {}", bookingId, userId);
+        return bookingService.getBookingById(bookingId, userId);
+    }
+
+    @GetMapping
+    public Collection<BookingDto> getBookingsByUser(@RequestParam(defaultValue = "ALL") String state,
+                                                    @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на получение списка бронирований пользователя с id = {} с параметром '{}'", userId, state);
+        return bookingService.getBookingsByUser(userId, state);
+    }
+
+    @GetMapping("/owner")
+    public Collection<BookingDto> getBookingsByOwner(@RequestParam(defaultValue = "ALL") String state,
+                                                     @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на получение списка бронирований владельца с id = {} с параметром '{}'", userId, state);
+        return bookingService.getBookingsByOwner(userId, state);
+    }
+}
---
+++ server/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -0,0 +1,74 @@
+package ru.practicum.shareit.item;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemDtoInput;
+import ru.practicum.shareit.item.dto.ItemDtoRequest;
+
+import java.util.Collection;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+    private final ItemService itemService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ItemDtoRequest createItem(@RequestBody ItemDtoInput dto,
+                                     @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на добавление новой вещи: {} пользователю с id = {}", dto.getName(), userId);
+        return itemService.createItem(userId, dto);
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(@PathVariable Long itemId,
+                              @RequestBody ItemDtoInput dto,
+                              @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на обновление вещи с id = {} у пользователя с id = {}", itemId, userId);
+        return itemService.updateItem(itemId, dto, userId);
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDto getItemById(@PathVariable Long itemId,
+                               @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на получение вещи с id = {} у пользователя с id = {}", itemId, userId);
+        return itemService.getItemById(itemId, userId);
+    }
+
+    @GetMapping
+    public Collection<ItemDto> getAllItemsByOwner(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на получение списка всех вещей пользователя с id = {}", userId);
+        return itemService.getAllItemsByOwner(userId);
+    }
+
+    @GetMapping("/search")
+    public Collection<ItemDto> searchItems(@RequestParam String text,
+                                           @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Запрос на поиск вещей пользователя с id = {} с текстом '{}'", userId, text);
+        return itemService.searchItems(text);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentDto createComment(@RequestBody CommentDto dto,
+                                    @RequestHeader("X-Sharer-User-Id") Long userId,
+                                    @PathVariable Long itemId) {
+        log.info("Запрос на добавление комментария к вещи с id = {} пользователем с id = {}", itemId, userId);
+        return itemService.createComment(itemId, userId, dto);
+    }
+}
